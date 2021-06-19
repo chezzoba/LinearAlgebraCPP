717889f1da2419d5aa071efe51ca76f1b410056c
EC2 Default User <ec2-user@ip-172-31-1-207.eu-west-1.compute.internal>
2021-06-19 17:50:32 +0000

    Commit to LinearAlgebraCPP branch main by Kaizad on June 19 at 17:50

--- main.cpp
+++ main.cpp
@@ -5,22 +5,6 @@
 using namespace std;
 using namespace arma;
 
-struct PointLoad {
-    vec p, f;
-
-    PointLoad (vec Point = zeros<vec>(3), vec forc = zeros<vec>(3)) {
-        p = Point;
-        f = forc;
-    };
-
-    vec force ( ) {
-        return f;
-    };
-
-    vec moment ( ) {
-        return cross(p, f);
-    };
-};
 
 int main ( ) {
     mat a = {1, 1, 1};
@@ -30,10 +14,5 @@ int main ( ) {
     b.i().print();
     (b.i() * vec({1, 2, 3})).print();
     cout << endl << endl;
-    PointLoad pl({1, 2, 3}, {4, 5, 6});
-    pl.force().print();
-    pl.moment().print();
-
-    (I % b).print();
     return 0;
 }
